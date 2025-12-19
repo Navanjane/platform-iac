0d920fba30fb31e5ff961d6e7a411274f77399d3
Navanjane <nkindelpitiya@gmail.com>
2025-12-19 06:53:47 +0000

    feat: conditional assume role

--- providers.tf
+++ providers.tf
@@ -1,7 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 
-  assume_role {
-    role_arn = "arn:aws:iam::147145168434:role/terraform-execution-role"
+  # Conditionally assume role based on TF_VAR_use_iam_role environment variable
+  # Set TF_VAR_use_iam_role=false for bootstrapping (initial deployment)
+  # Set TF_VAR_use_iam_role=true for normal operations (default)
+  dynamic "assume_role" {
+    for_each = var.use_iam_role ? [1] : []
+    content {
+      role_arn = "arn:aws:iam::147145168434:role/terraform-execution-role"
+    }
   }
 }
