40fe9090f6fab8317b4775851f6f7418a9672ec4
navanjane <nkindelpitiya@gmail.com>
2026-01-11 19:36:27 +0530

    feat: argocd ingress

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -7,8 +7,28 @@ resource "helm_release" "argocd" {
   create_namespace = var.create_namespace
   version          = var.chart_version
 
-  values = var.values_file != "" ? [
-    file("${path.module}/values/argocd.yaml"),
-    file(var.values_file)
-  ] : [file("${path.module}/values/argocd.yaml")]
+  # Use different values files based on ingress enablement
+  values = var.enable_ingress ? (
+    var.values_file != "" ? [
+      file("${path.module}/values/argocd-secure.yaml"),
+      file(var.values_file)
+    ] : [file("${path.module}/values/argocd-secure.yaml")]
+  ) : (
+    var.values_file != "" ? [
+      file("${path.module}/values/argocd.yaml"),
+      file(var.values_file)
+    ] : [file("${path.module}/values/argocd.yaml")]
+  )
+
+  # Set domain dynamically if ingress is enabled
+  set = var.enable_ingress && var.domain_name != "" ? [
+    {
+      name  = "global.domain"
+      value = var.domain_name
+    },
+    {
+      name  = "server.config.url"
+      value = "https://${var.domain_name}"
+    }
+  ] : []
 }
